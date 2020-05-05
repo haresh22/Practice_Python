b6cb7059b7a6d68a41e3ff5ae1c47ccc0a7f3c6a
Haresh Singh <harishrsingh22@gmail.com>
2020-05-06 00:34:29 +0530

    a

--- print_command_formating.py
+++ print_command_formating.py
@@ -1,11 +1,23 @@
 ############### Print command Formatting ######################
 
 
-#a = "Pentaafood is an fast food Chain Company "
-
-#print(a.format())
-
 name = "Haresh"
 age = 34
 
-print("%s is %d year old " % (name, age))
+print("Hi, %s your %d is" % (name,age))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
