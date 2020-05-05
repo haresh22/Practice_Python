dbaef2b25fa97c33e2af67d77b90073bbe168124
Haresh Singh <harishrsingh22@gmail.com>
2020-05-05 20:47:33 +0530

    ver 1.1

--- list.py
+++ list.py
@@ -5,5 +5,17 @@ Product_list = ["Pizza Bread", "Burger Bun", "Garlic Bread", "Coldrinks", ]
 Sauce_List = ["White Sauce","Mayoniease","Cheese Sauce","Barbeque Sauce" ]
 
 
+Employe_list.append("Neha") # Append Prtice 
+Product_list.append("Milk") # Append Pratice 
+Sauce_List.append("Tomato-Ketchup") # Append Pratice
+
+
+print(Sauce_List)
+print(Product_list)
+print(Employe_list)
+
+
+
+
 
 
