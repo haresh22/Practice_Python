050ad00903f966e33db0a0c1764fb341e9699097
Haresh Singh <harishrsingh22@gmail.com>
2020-05-05 17:53:41 +0530

    1.0

---
+++ striing_sliceing.py
@@ -0,0 +1,15 @@
+########### Sring Sliceing in Python #################
+
+mystrng = "Haresh Singh"
+print(mystrng[0])  # index is start with ZERO in Every string 
+print(mystrng[1])
+print(mystrng[2])
+print(mystrng[3])
+print(mystrng[4])
+print(mystrng[5])
+print(mystrng[6]) # Indexy calculate the SPACE also [ ]
+print(mystrng[7])
+print(mystrng[8])
+print(mystrng[9])
+print(mystrng[10])
+print(mystrng[11])
