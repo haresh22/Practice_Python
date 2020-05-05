e58d595d9bf18b07f420c5acd9f342da171de2d2
Haresh Singh <harishrsingh22@gmail.com>
2020-05-05 17:43:18 +0530

    1.0

---
+++ string.py
@@ -0,0 +1,24 @@
+###########  Type "String Pratice" ##################
+
+mystrng1 = "Haresh"
+mystrng2 = "Haresh Singh"
+mystrng3 = 'hareshsingh'
+mystrng4 = "1234567890"
+mystrng5 = "Haresh Singh 1234567890"
+mystrng6 = "Haresh is good Boy and he is working with Coverfox "
+
+print(mystrng1)
+print(mystrng2)
+print(mystrng3)
+print(mystrng4)
+print(mystrng5)
+print(mystrng6)
+################ Type Intejar this is Pure Numbers #################
+
+newstrng1 = int(1234567890)
+newstrng2 = int(987654321) #you cant start with ZERO ########
+newstrng3 = float(10.12)
+newstrng4 = float(0.11)
+newstrng5 = float(0.1) 
+
+
