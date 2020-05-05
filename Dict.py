5b9ccfc692f0bb738baf0374f2d4462b32d681b1
Haresh Singh <harishrsingh22@gmail.com>
2020-05-05 20:19:03 +0530

    ver 1.0

---
+++ Dict.py
@@ -0,0 +1,11 @@
+######## DICT ####################
+
+pentaafood_Emp_details = {
+    "Haresh":["Mobile No-9820953085","haresh@pentaafood.com","Salary - 10,000"], # first Key then : then Value of list 
+    "Vipula":["998899889","vipula@pentaafood.com","Salalry - 13,000"],
+    "Vikrant":["88118811","vikrant@pentaafood.com","Salary - 9,000"],
+}
+
+print(pentaafood_Emp_details["Haresh"][0])
+print(pentaafood_Emp_details["Vikrant"][2])
+print(pentaafood_Emp_details["Vipula"][1])
