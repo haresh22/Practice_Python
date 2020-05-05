781ed60523bff430938fe4cde8c49b62ca4f2729
Haresh Singh <harishrsingh22@gmail.com>
2020-05-05 18:32:48 +0530

    1.0

--- list.py
+++ list.py
@@ -1,3 +1,10 @@
 ############ LIST in PYTHON ##############
 
+Employe_list = ["Haresh Singh", "haresh@coverfoxmail.com", "9820953085", "IT"]
+
+print(Employe_list[0])
+print(Employe_list[1])
+print(Employe_list[2])
+print(Employe_list[3])
+
 
