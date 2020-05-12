19a75bf7ce245205fd8912022e00082fe1a091f7
Haresh Singh <harishrsingh22@gmail.com>
2020-05-12 13:38:35 +0530

    1.0

--- 1.py
+++ 1.py
@@ -3,4 +3,5 @@ mobile = int(input("Enter your mobile no :"))
 gender = input("Enter your Gender :")
 user_details = f"Hi {user_name} your mobile no is {mobile} and you are {gender}!"
 
-print(user_details)
+print(user_details.title())
+
